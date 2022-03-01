8e9641680dc646b5edf3ec21464e413e2c6b1744
qian <1240562101@qq.com>
2022-03-01 21:48:38 +0800

    third commit

--- src/main/java/com/atgugu/test/GitTest.java
+++ src/main/java/com/atgugu/test/GitTest.java
@@ -7,6 +7,9 @@ public class GitTest {
 
         System.out.println("hello git1");
         System.out.println("hello git2");
+
+        System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 
 }
